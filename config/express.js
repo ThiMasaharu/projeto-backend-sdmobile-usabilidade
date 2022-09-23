72eb15161488e89d3830e3d4e90168aacecb1397
Luukitas <lucas.kabral2112@gmail.com>
2022-09-23 20:49:15 -0300

    Ajuste CORS para comunicação com front-end

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
+var cors = require('cors')
 
 module.exports = () => {
     const app = express();
+    app.use(cors())
 
     app.set('port', 8080)
 
