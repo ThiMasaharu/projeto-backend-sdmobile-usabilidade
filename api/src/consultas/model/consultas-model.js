3ebf8a190724c3b7a13c3e1e3ac8102baaffdb60
Luukitas <lucas.kabral2112@gmail.com>
2022-11-24 21:42:47 -0300

    Ajuste na model consulta

--- api/src/consultas/model/consultas-model.js
+++ api/src/consultas/model/consultas-model.js
@@ -5,6 +5,7 @@ const Consulta = new Schema({
     medico: {type: Object, required: false},
     paciente: {type: Object, required: false},
     data: {type: String, required: false},
+    hora: {type: String, required: false},
     descricao: {type: String, required: false}
 })
 
